cd8f9632afd90a9214047c5318caa1cdc31f611e
evencoding <88191233+evencoding@users.noreply.github.com>
2021-09-03 08:49:17 +0900

    # 2021-09-03

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/mytube", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("🖐  Connected to DB ");
+const handleError = (error) => console.log("❌  DB Error:", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import "./db";
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -8,15 +10,16 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`🖐  Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
