cd81d174067e12911546431ea948bbb432284c08
evencoding <88191233+evencoding@users.noreply.github.com>
2021-10-21 20:09:45 +0900

    # 2021-10-21_2

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
         },
       },
       {
-        test: /\.scss$/,
+        test: /\.sass$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
